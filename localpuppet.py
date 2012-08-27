4cc7c1b6501a631bdda1566963feeea28fbdfc28
Robert M. Thomson <rmt-git@corporatism.org>
2012-08-27 07:08:32 +0000

    add GPL-2 license

--- localpuppet.py
+++ localpuppet.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
+#
+# Example script to do a standalone puppet run with a custom modulepath
+# based on versioned application recipes and an ENC.
+#
+# Copyright 2012  Robert Thomson
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#
 """
 This script will let you run puppet standalone with
 support for multiple environments (eg. version'd environments)
